e7d23b682a884adbac468a5b11a3eb82606fe5b9
Tânia Assis <taniafilipalopes@gmail.com>
2016-12-29 00:44:16 +0000

    Adicionar
    Primeiras Verificações Adicionar

--- GestaoContactos/public_html/JS/Adicionar.js
+++ GestaoContactos/public_html/JS/Adicionar.js
@@ -29,29 +29,27 @@ function alterar(){
     }
 }
 
+
 function guardarLocalStorage(){
     var s = JSON.stringify(Contactos);
     localStorage.setItem('contacto', s);              //guardar os albuns
 }
 
-function ListarContacto () {
-    
-    alert(Contactos[Contactos.length-1].nome);
-    alert(Contactos[Contactos.length-1].email);
-    alert(Contactos[Contactos.length-1].pais);
-    alert(Contactos[Contactos.length-1].telefone);
-    alert(Contactos[Contactos.length-1].tipo);
-    alert(Contactos[Contactos.length-1].DOB);
-    alert(Contactos[Contactos.length-1].Facebook);
-    alert(Contactos[Contactos.length-1].Google);
-    alert(Contactos[Contactos.length-1].LinkedIn);
-    alert(Contactos[Contactos.length-1].Instagram);
-    alert(Contactos[Contactos.length-1].Outro);
-    alert(Contactos[Contactos.length-1].Obs);
-    alert(Contactos[Contactos.length-1].Grupo);
-    alert(Contactos[Contactos.length-1].Fav);
-        
-    guardarLocalStorage();
+function ListarContacto (pos) {
+    alert(Contactos[pos].nome);
+    alert(Contactos[pos].email);
+    alert(Contactos[pos].pais);
+    alert(Contactos[pos].telefone);
+    alert(Contactos[pos].tipo);
+    alert(Contactos[pos].DOB);
+    alert(Contactos[pos].Facebook);
+    alert(Contactos[pos].Google);
+    alert(Contactos[pos].LinkedIn);
+    alert(Contactos[pos].Instagram);
+    alert(Contactos[pos].Outro);
+    alert(Contactos[pos].Obs);
+    alert(Contactos[pos].Grupo);
+    alert(Contactos[pos].Fav);
 }
 
 function isFavorito () {
@@ -64,41 +62,113 @@ function isFavorito () {
 }
 
 function AdicionarContacto() {
-    
-        alert("2");
-        alert(Contactos[0].nome);
-        var nome = document.getElementById('Nome');
-        var email = document.getElementById('Email');
-        var pais = document.getElementById('Pais');
-        var telefone = document.getElementById('Numero');
-        var tipo = document.getElementById('Tipo');
-        var DOB = document.getElementById('DOB');
-        var facebook = document.getElementById('Facebook');
-        var google = document.getElementById('Google');
-        var linkedIn = document.getElementById('LinkedIn');
-        var instagram = document.getElementById('Instagram');
-        var outro = document.getElementById('Outro');
-        var obs = document.getElementById('Obs');
-        var grupo = document.getElementById('Grupo');
-        var fav = document.getElementById('Fav');
         
-        Contactos.push({nome: nome.value, email: email.value, pais: pais.value, telefone: telefone.value, tipo: tipo.value, DOB: DOB.value, Facebook: facebook.value, Google: google.value, LinkedIn: linkedIn.value, Instagram: instagram.value, Outro: outro.value, Obs: obs.value, Grupo: Grupos[0], Fav: fav});
-  
-        ListarContacto();
-    
+        alert("3");
+        var nome = document.getElementById('Nome');
+        var telefone = document.getElementById('Numero');
+        var aux=0;
+        for (var i=0; i<Contactos.length; i++){
+            alert("4");
+            if (telefone.value === Contactos[i].numero){
+                alert("5");
+                alert("Número já existe!");
+                aux=1;
+            }
+            if (nome.value === Contactos[i].nome){
+                alert("6");
+                alert("Nome já existe!");
+                aux=1;
+            }
+        }
+        alert("6");
+        if (aux == 0){  
+            alert("7");
+            var email = document.getElementById('Email');
+            var pais = document.getElementById('Pais');
+            var tipo = document.getElementById('Tipo');
+            var DOB = document.getElementById('DOB');
+            var facebook = document.getElementById('Facebook');
+            var google = document.getElementById('Google');
+            var linkedIn = document.getElementById('LinkedIn');
+            var instagram = document.getElementById('Instagram');
+            var outro = document.getElementById('Outro');
+            var obs = document.getElementById('Obs');
+            var grupo = document.getElementById('Grupo');
+            var fav = document.getElementById('Fav');
+            alert("8");
+            Contactos.push({nome: nome.value, email: email.value, pais: pais.value, telefone: telefone.value, tipo: tipo.value, DOB: DOB.value, Facebook: facebook.value, Google: google.value, LinkedIn: linkedIn.value, Instagram: instagram.value, Outro: outro.value, Obs: obs.value, Grupo: Grupos[0], Fav: fav});
+            alert("9");
+            ListarContacto(Contactos.length-1);
+            
+            guardarLocalStorage();
+        }else{
+            alert("asfhlernf");
+        }
 }
 
 function AbrirLocalStorage() {
     if (localStorage.contacto != null){
         Contactos = JSON.parse(localStorage.contacto);
-        alert("1");
+        alert("2");
         AdicionarContacto();
     }else{
-        var vazio=document.createTextNode("Não existem Contactos");
+        var vazio=document.createTextNode("Impossível adicionar Contactos");
         var erro= document.getElementById("Erro");
         alert(erro);
         erro.appendChild(vazio);
     }
+}
+function validarCampoNumero(inputNumero){ // verifica se o Campo Nome esta vazio
+    if (inputNumero.value === ""){ // Esta Vazio
+        inputNumero.className = "inputError"; //Span //campo vazio -- só de inputInvalido
+        document.getElementById('inputNumeroInvalido').className = "inputMessageError"; // Mostra o Span Error
+        return false; // Nao Aceita esta Campo
+    }else{
+        inputNumero.className = ""; // Default
+        document.getElementById('inputNumeroInvalido').className = "inputMessageErrorHidden"; // Esconde a menssagem Erro
+    }
+    return true; // Aceitou este Campo
+}
+
+function validarCampoNome(inputNome){ // verifica se o Campo Nome esta vazio
+    if (inputNome.value === ""){ // Esta Vazio
+        inputNome.className = "inputError"; //Span //campo vazio -- só de inputInvalido
+        document.getElementById('inputNomeInvalido').className = "inputMessageError"; // Mostra o Span Error
+        return false; // Nao Aceita esta Campo
+    }else{
+        inputNome.className = ""; // Default
+        document.getElementById('inputNomeInvalido').className = "inputMessageErrorHidden"; // Esconde a menssagem Erro
+    }
+    return true; // Aceitou este Campo
+}
+
+function VerificaCampos(){ // verifica se os campos estao vazios
+    
+    var flag = 0;
+    var inputNome = document.getElementById('Nome'); // recebe o parametro do input Nome 
+    
+    var inputNumero = document.getElementById('Numero'); // recebe o parametro do input Numero
+    
+    while(flag == 0){
+        
+        if (validarCampoNome(inputNome)===true) flag++; // flag = 1
+        else if (validarCampoNumero(inputNumero)===true) flag++; // flag = 2
+        
+        else flag = -1; // sai do ciclo mas sem sucesso
+        
+    }
+    
+    //if (validarCampoNome(inputNome)===true && validarCampoNumero(inputNumero)===true){
+    if(flag == 2){    
+        
+        AbrirLocalStorage();
+        
+    }
+        
+    
+    
+    //adicionarComentario(inputNome.value);
+    
 }
 
 function init(){
@@ -106,8 +176,21 @@ function init(){
     var color = document.getElementById('cor');
     color.addEventListener('change', alterar);
     
-    var btn = document.getElementById('Enviar');
-    btn.addEventListener('click', AbrirLocalStorage);
+    alert("1 - init");
+    var btnEnviar = document.getElementById('Enviar');
+    btnEnviar.addEventListener('click', VerificaCampos);
+    
+    /*
+    var nome = document.getElementById('Nome');
+    var nometmp;
+    
+    if (validarNome(nome)===true){
+        nometmp = nome.value;
+    }
+    
+    adicionarContacto(nometmp);
+    AbrirLocalStorage
+    */
 }
 
 document.addEventListener('DOMContentLoaded', init);
