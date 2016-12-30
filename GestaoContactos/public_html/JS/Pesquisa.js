fea63f2230e44537308c713dcc17238b8fa5a099
Tânia Assis <taniafilipalopes@gmail.com>
2016-12-30 12:10:53 +0000

    Pesquisa
    Comecei a fazer

---
+++ GestaoContactos/public_html/JS/Pesquisa.js
@@ -0,0 +1,185 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var Contactos = [];
+
+function alterar(){
+    var cor = document.getElementById('cor');
+    var link = document.getElementById('link');
+    if (cor.value == 'cornsilk'){
+        fundo='cornsilk';
+        link.href="styles/cornsilk.css";
+    }else{
+        if(cor.value == 'bisque'){
+            fundo='bisque';
+            link.href="styles/bisque.css";
+        }else{
+            if(cor.value == 'burlywood'){
+                fundo='burlywood';
+                link.href="styles/burlywood.css";
+            }else{
+                if(cor.value == 'peachpuff'){
+                    fundo='peachpuff';
+                    link.href="styles/peachpuff.css";
+                }
+            }
+        }
+    }
+    guardarLocalStorage();
+}
+
+function CorFundo(){
+    var cor = document.getElementById('cor');
+    var link = document.getElementById('link');
+    if (fundo == 'cornsilk'){
+       document.getElementById(fundo).selected="selected";
+        link.href="styles/cornsilk.css";
+    }else{
+        if(fundo == 'bisque'){
+           document.getElementById(fundo).selected="selected";
+           link.href="styles/bisque.css";
+        }else{
+            if(fundo == 'burlywood'){
+                document.getElementById(fundo).selected="selected";
+                link.href="styles/burlywood.css";
+            }else{
+                if(fundo == 'peachpuff'){
+                    document.getElementById(fundo).selected="selected";
+                    link.href="styles/peachpuff.css";
+                }
+            }
+        }
+    }
+}
+
+
+function guardarLocalStorage(){
+    var s = JSON.stringify(Contactos);
+    localStorage.setItem('contacto', s);              //guardar os albuns
+    localStorage.setItem('fundo',fundo);
+}
+
+function guardarLocalStorageContact(indice){
+    var s = JSON.stringify(Contactos);
+    localStorage.setItem('contacto', s);              //guardar os albuns
+    localStorage.setItem('fundo',fundo);
+    localStorage.setItem('indice',indice);
+}
+
+function OrdenarContactos(a, b) {
+    if(a.nome>b.nome){
+         return 1;
+    }
+    if(a.nome<b.nome){
+         return -1;
+    }
+    return 0;
+}
+
+function ObterIndice(array, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i].nome === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function removerContacto(){
+    var conf=confirm ("Tem a certeza que deseja remover?");
+    if(conf==true){
+        var x = this.parentNode.getElementsByTagName("p")[0].getAttribute("id"); 
+        var indice = ObterIndice(Contactos,x);
+        if (indice > -1) {
+            Contactos.splice(indice, 1);
+            window.location.reload();
+        }else{
+            alert("erro ao remover!!");
+        }
+        guardarLocalStorage();
+    }
+    
+}
+
+function editarContacto(){
+    var x = this.parentNode.getElementsByTagName("p")[0].getAttribute("id"); 
+    var indice = ObterIndice(Contactos,x);
+    guardarLocalStorageContact(indice);
+    window.location.href="Editar.html";
+}
+
+function AdicionarFavoritos() {
+    
+    var x = this.parentNode.getElementsByTagName("p")[0].getAttribute("id"); 
+    var indice = ObterIndice(Contactos,x);
+    if (Contactos[indice].Fav == "sim") {
+        Contactos[indice].Fav = "nao";
+        alert("Removido dos Favoritos com Sucesso!");
+        window.location.reload();
+    }else{
+        Contactos[indice].Fav = "sim";
+        alert("Adicionado aos Favoritos com Sucesso!");
+        window.location.reload();
+    }
+        
+    guardarLocalStorage();
+}
+
+function PesquisarContacto(){
+    Contactos.sort(OrdenarContactos);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+function Voltar(){
+    window.location.href="index.html";
+}
+
+function AbrirLocalStorage() {
+    if (localStorage.contacto != null){
+        Contactos = JSON.parse(localStorage.contacto);
+        fundo= localStorage.fundo;
+        PesquisarContacto();
+    }else{
+        var vazio=document.createTextNode("Não existem Contactos");
+        var erro= document.getElementById("Erro");
+        alert(erro);
+        erro.appendChild(vazio);
+    }
+}
+
+function init(){
+    AbrirLocalStorage();
+    CorFundo();
+    var btnVoltar = document.getElementById('Voltar');
+    btnVoltar.addEventListener('click', Voltar);
+    var color = document.getElementById('cor');
+    color.addEventListener('change', alterar);   
+}
+document.addEventListener('DOMContentLoaded', init);
